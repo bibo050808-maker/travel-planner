917adaa1ac076061bcc55aef820fc3903210cb2c
travel-planner <travel@example.com>
2026-06-10 21:36:55 +0800

    Fix: use named imports for React to prevent tree-shaking issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
 import { AppProvider } from './store/AppContext'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <HashRouter>
       <AppProvider>
         <App />
       </AppProvider>
     </HashRouter>
-  </React.StrictMode>
+  </StrictMode>
 )
